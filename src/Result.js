d8024c31beda1d46ce7aec789e7a683a4f5f14f0
z <z@192.168.11.60>
2024-11-30 11:42:07 +0300

    Initial commit

--- src/Result.js
+++ src/Result.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './App.css'; 
 
 const Result = ({ onRestart }) => {
+  // Генерация случайного семизначного числа
+  const randomID = Math.floor(1000000 + Math.random() * 9000000);
+
   return (
     <div className="result-container">
       <h2>Тест завершен</h2>
-      <p>Спасибо, что потратили своё время! Вы можете пройти тест ещё раз.</p>
+      <p>Спасибо, что потратили своё время!</p>
+        <p>Вы можете пройти тест ещё раз.</p>
+      <p>Ваш ID: {randomID}</p> {/* Отображаем случайный номер ID */}
       <button onClick={onRestart}>Начать заново</button>
     </div>
   );
